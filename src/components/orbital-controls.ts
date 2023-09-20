a56ad089ef38201f741ae2db47b99033d9bf0072
Taras Paliy <taras505paliy@gmail.com>
2023-09-20 16:06:57 +0300

    fixing import

--- src/components/orbital-controls.ts
+++ src/components/orbital-controls.ts
@@ -1,4 +1,4 @@
-import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 function createControls(camera, element) {
   const controls = new OrbitControls(camera, element);
